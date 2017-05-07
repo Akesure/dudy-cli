4edc867e8e336c87a4caab9f43802278940954a6
weimeng <weimeng@wm.local>
2017-05-08 00:04:49 +0800

    dudy updates

--- add_npm_script.js
+++ add_npm_script.js
@@ -4,7 +4,7 @@ function add_npm_script(file) {
   const json = JSON.parse(fs.readFileSync(file, "utf-8"))
 
   json.scripts = {
-    dev : "NODE_ENV=development ./build-scripts/webpack.build.js -e",
+    dev : "dudy -i ./src && NODE_ENV=development ./build-scripts/webpack.build.js -e",
     build: "NODE_ENV=publish ./build-scripts/webpack.build.js -e"
   }
 
